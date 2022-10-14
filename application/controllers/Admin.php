ddf87a3eb7fc653f882c2011e736bb69908e0e2b
jibar37 <111734110+jibar37@users.noreply.github.com>
2022-10-14 22:03:04 +0800

    fix session

--- application/controllers/Admin.php
+++ application/controllers/Admin.php
@@ -6,28 +6,34 @@ class Admin extends CI_Controller
 
     public function index()
     {
-        $username = $this->input->post('username');
-        $password = $this->input->post('password');
-        $data_session = array(
-            'nama' => $username,
-            'status' => "login"
-        );
-        $this->session->set_userdata($data_session);
-        $data['nama'] = $this->session->userdata('nama');
+        $isSession = $this->session->userdata('nama');
+        if (empty($isSession)) {
+            $username = $this->input->post('username');
+            $password = $this->input->post('password');
+            $data_session = array(
+                'nama' => $username,
+                'status' => "login"
+            );
+            $this->session->set_userdata($data_session);
+            $data['nama'] = $this->session->userdata('nama');
 
-        $data['tittle'] = 'ADMIN';
-        $this->load->view('navbar\header', $data);
-        // $this->load->view('admin\dashboard', $data);
+            $data['tittle'] = 'ADMIN';
+            $this->load->view('navbar\header', $data);
+            $this->load->view('admin\dashboard', $data);
+            $this->load->view('navbar\footer');
+        } else {
+            $data['tittle'] = 'ADMIN';
+            $data['nama'] = $this->session->userdata('nama');
 
-        $this->load->view('map');
-        $this->load->view('navbar\footer');
-        echo ($this->session->userdata('status'));
-        echo ($password);
+            $this->load->view('navbar\header', $data);
+            $this->load->view('admin\dashboard', $data);
+            $this->load->view('navbar\footer');
+        }
     }
     public function signIn()
     {
-        $username = $this->input->post('username');
-        $password = $this->input->post('password');
+        // $username = $this->input->post('username');
+        // $password = $this->input->post('password');
         // $where = array(
         // 	'username' => $username,
         // 	'password' => md5($password)
@@ -35,12 +41,12 @@ class Admin extends CI_Controller
         // $cek = $this->m_login->cek_login("admin",$where)->num_rows();
         // if($cek > 0){
 
-        $data_session = array(
-            'nama' => $username,
-            'status' => "login"
-        );
+        // $data_session = array(
+        //     'nama' => $username,
+        //     'status' => "login"
+        // );
 
-        $this->session->set_userdata($data_session);
+        // $this->session->set_userdata($data_session);
 
         // 	redirect(base_url("admin"));
 
@@ -48,7 +54,7 @@ class Admin extends CI_Controller
         // 	echo "Username dan password salah !";
         // }
         $data['tittle'] = 'ADMIN';
-        $data['nama'] = "jihad";
+        $data['nama'] = $this->session->userdata('nama');
 
         $this->load->view('navbar\header', $data);
         $this->load->view('admin\dashboard', $data);
--- application/views/dashboard.php
+++ application/views/dashboard.php
@@ -1,18 +1,3 @@
-<!-- <!DOCTYPE html>
-<html lang="en">
-<head>
-	<meta charset="utf-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-	<link rel="stylesheet" href=assets/tambahan.css />
-	<link rel="stylesheet" href="https://unpkg.com/leaflet@1.7.1/dist/leaflet.css" />
-	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.2.1/dist/css/bootstrap.min.css" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
-	
-	<script src="https://unpkg.com/leaflet@1.7.1/dist/leaflet.js" integrity="sha512-XQoYMqMTK8LvdxXYG3nZ448hOEQiglfqkJs1NOQV44cWnUrBc8PkAOcXy20w0vlaXaVUearIOBhiXZ5V3ynxwA==" crossorigin=""></script>
-	<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-	<script src="https://cdn.jsdelivr.net/npm/popper.js@1.14.3/dist/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
-	<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.1.3/dist/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
-	<title>LAND ZONe</title>
-</head> -->
 <div class="d-flex flex-column flex-md-row align-items-center p-3 px-md-4 mb-3 bg-white border-bottom box-shadow">
 	<h5 class="my-0  font-weight-bold">LAND ZONe</h5>
 
@@ -24,7 +9,7 @@
 		<a class="btn btn-outline-primary" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
 			LOGIN
 		</a>
-		<form class="dropdown-menu p-4" action="<?php echo base_url('admin'); ?>" method="post">
+		<form class="dropdown-menu p-4" action=<?php echo (base_url('admin')); ?> method="post">
 			<div class="form-group">
 				<label for="username">Username</label>
 				<input type="text" class="form-control" name="username" id="username" placeholder="Username">
